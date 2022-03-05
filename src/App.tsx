12a7c30bbaa6b7d5b3d6419df838a0dae0eddb7d
前山大次郎 <daijiro.maeyama13@gmail.com>
2022-03-05 18:45:27 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+const App: React.FC = () => {
+  const [status, setState] = useState<string | number>('text')
+  const [input, setInput] = useState("")
+  const [counter, setCounter] = useState(0)
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value)
+  }
+  useEffect(() => {
+    console.log("useEffect in App invorked!")
+    document.title = `current value is ${counter}`
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h4>{status}</h4>
+        <button onClick={() => setState(1)}>Button</button>
+        <h4>{input}</h4>
+        <input type='text' value={input} onChange={onChangeHandler}/>
+        <h4>{counter}</h4>
+        <button onClick={() => setCounter((preCounter)=>preCounter+1)}>
+          Increment
+        </button>
+        <Counter />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
